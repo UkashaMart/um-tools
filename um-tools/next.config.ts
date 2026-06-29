80a4f576de79f0997e0b9b7e3d8a88a0f7218831
ukasha-altaf <ukashaaltaf0@gmail.com>
2026-06-29 09:15:15 +0500

    remove webpack config - not compatible with turbopack

--- um-tools/next.config.ts
+++ um-tools/next.config.ts
@@ -4,10 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
-  webpack: (config) => {
-    config.experiments = { ...config.experiments, asyncWebAssembly: true };
-    return config;
-  },
 };
 
 export default nextConfig;
